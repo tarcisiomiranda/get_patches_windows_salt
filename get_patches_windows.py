9e186cfbf7e21c7d87b911479f06c3be8daefc44
tarcisio <eu@tarcisio.me>
2022-10-06 14:46:07 -0300

    add json return

--- get_patches_windows.py
+++ get_patches_windows.py
@@ -20,12 +20,14 @@ expression={switch($_.operation){ 1 {"Installation"}; 2 {"Uninstallation"}; 3 {"
 | Out-File C:\\temp\scripts\\getkb.json -Encoding utf8 \
 | Set-Content -Encoding UTF8 C:\\temp\\scripts\\getkb.json; \
 Get-Content C:\\temp\\scripts\\getkb.json"""
+
+    # call salt
     jid = client.cmd_async('kernel:Windows', 'cmd.run', [cmd], kwarg={'shell' : 'powershell'}, tgt_type='grain')
     time.sleep(10)
     res = client.get_cache_returns(jid)
+    # handling the data
+    newkb = {}
     ret_data = {}
-    newkb = []
-    newkb2 = {}
     for minion_id in res:
         print(minion_id, '\n')
         minion_patches = []
@@ -35,18 +37,18 @@ Get-Content C:\\temp\\scripts\\getkb.json"""
         minion_patches2 = {}
         minion_patches3 = []
         for path in patches:
-            # Get Time
+            # get Time
             date = path.get('Date')
             GETDATE = re.compile('[0-9]{1,}', re.IGNORECASE)
             sdate = GETDATE.search(date)
             sdate = sdate.group(0) if sdate else 'NA'
             date = datetime.fromtimestamp(int(sdate)//1000)
-            # Get KB
+            # get KB
             GETKB = re.compile('KB[0-9]{6,7}', re.IGNORECASE)
             title = path['Title']
             kbl = GETKB.search(title)
             kbl = kbl.group(0) if kbl else 'NA'
-            # Novo objeto
+            # new obj
             newpatches = {'kb': kbl, 'installed_on': str(date)}
 
             if not kbl in minion_patches2:
@@ -62,7 +64,8 @@ Get-Content C:\\temp\\scripts\\getkb.json"""
             minion_patches_sorted = sorted(value, key=key_func, reverse=True)
             minion_patches3.append(minion_patches_sorted[0])
     
-    newkb2[minion_id] = minion_patches3
-    print(newkb2)
+    newkb[minion_id] = minion_patches3
+    return json.dumps(newkb, indent=2)
 
-coleta()
+# Call the script
+print(coleta())
